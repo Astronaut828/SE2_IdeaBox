be125303570d7d94264cd00034ca80e12f5fde50
quizford <sociabarbernet@gmail.com>
2024-12-23 08:06:39 -0700

    WIP: DB setup with redis / upstash

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import UserAuth from "~~/components/UserAuth";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -38,7 +39,7 @@ const Home: NextPage = () => {
             </code>
           </p>
         </div>
-
+        <UserAuth />
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
